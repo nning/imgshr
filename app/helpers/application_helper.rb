cff4baa4ff96dc49dd6e46918f9b023f20fc5d4c
henning mueller <henning@orgizm.net>
2014-07-28 11:38:28 +0200

    A little user interface work.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def brand
+    content_tag :div, class: 'imgshr-brand' do
+      icon(:picture, class: 'imgshr-icon') + 'IMGSHR'
+    end
+  end
+
   # Convert flash message type to bootstrap class.
   def flash_class(type)
     ({notice: :info, alert: :warning, error: :danger}[type.to_sym] || type).to_s
@@ -7,6 +13,6 @@ module ApplicationHelper
   # Helper for glyphicon span tags.
   def icon(name, options = {})
     name = name.to_s.gsub(/_/, '-')
-    content_tag :span, nil, class: "glyphicon glyphicon-#{name} #{options[:class]}"
+    content_tag :span, nil, class: "glyphicon glyphicon-#{name} #{options[:class]}", id: options[:id]
   end
 end
