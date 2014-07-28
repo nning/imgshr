151af8c45afac5746523b990ff7daa4f37b9ebef
henning mueller <henning@orgizm.net>
2014-07-28 22:17:49 +0200

    Changed link to gallery text in feed item.

--- app/helpers/galleries_helper.rb
+++ app/helpers/galleries_helper.rb
@@ -1,6 +1,6 @@
 module GalleriesHelper
   def feed_item_content(picture)
     image_tag(picture.image.url(:medium), title: picture) +
-      content_tag(:p, link_to("Visit #{@gallery} on IMGSHR!", @gallery))
+      content_tag(:p, link_to("This picture is part of the #{@gallery} gallery on IMGSHR.", @gallery))
   end
 end
