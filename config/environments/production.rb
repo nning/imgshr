b2f6c1b8b933d1a17f65e70e0869b69253611df4
henning mueller <henning@orgizm.net>
2015-06-09 18:07:01 +0200

    Deactivate logging of views.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Deactivate logging of views.
+  config.action_view.logger = nil
 end
