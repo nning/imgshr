1224ea5884d8219cf6cc52891e6f0cfe7cf8044d
henning mueller <henning@orgizm.net>
2015-07-01 22:57:16 +0200

    skip_boss_token method for BossTokenAble module.

--- lib/boss_token_able.rb
+++ lib/boss_token_able.rb
@@ -2,6 +2,7 @@ module BossTokenAble
   def self.included(base)
     base.class_eval do
       before_action :boss_token, only: [:show]
+      base.extend BossTokenAble
     end
   end
 
@@ -12,6 +13,8 @@ module BossTokenAble
   protected
 
   def boss_token
+    return if skip_boss_token?
+
     if session["boss_token_#{gallery.slug}"]
       @boss_token ||= BossToken.find_by_slug(session["boss_token_#{gallery.slug}"])
     end
@@ -23,5 +26,26 @@ module BossTokenAble
 
   def enforce_read_only
     head :forbidden unless writable?
+  end
+
+  def skip_boss_token(*actions)
+    @@_skip_boss_token ||= {}
+
+    actions.each do |action|
+      @@_skip_boss_token[self] ||= []
+      @@_skip_boss_token[self] << action.to_s
+    end
+  end
+
+  def skip_boss_token?
+    return false unless defined? @@_skip_boss_token
+
+    controller_entry = @@_skip_boss_token[self.class]
+
+    if controller_entry && controller_entry.include?(params[:action])
+      return true
+    end
+
+    false
   end
 end
