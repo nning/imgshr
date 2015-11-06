e21c802ae1c01b9a45cd45fe998cc8e2b5c42371
henning mueller <henning.mueller@hmmh.de>
2015-11-06 11:28:50 +0100

    Configurable random token length.

--- lib/random_string.rb
+++ lib/random_string.rb
@@ -2,8 +2,9 @@ require 'securerandom'
 
 module RandomString
   ALPHANUMERIC = [*('a'..'z'), *('A'..'Z'), *(0..9)].freeze
+  TOKEN_LENGTH = ::Settings.token_length || 8
 
-  def self.generate(size = 8, set = ALPHANUMERIC)
+  def self.generate(size = TOKEN_LENGTH, set = ALPHANUMERIC)
     l = set.size
     (0...size).map { set[SecureRandom.random_number(l)] }.join
   end
