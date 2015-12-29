878b81b1125459aacebc413234e4e6207b9cce89
henning mueller <mail@nning.io>
2015-12-29 14:20:25 +0100

    Sidekiq job for deletion of temporary links.

---
+++ app/jobs/invalidate_temp_links.rb
@@ -0,0 +1,7 @@
+class InvalidateTempLinks < ActiveJob::Base
+  queue_as :temp_link_invalidation
+
+  def perform(temp_link)
+    TempLink.find_by_slug!(temp_link.slug).destroy!
+  end
+end
--- app/models/temp_link.rb
+++ app/models/temp_link.rb
@@ -1,5 +1,16 @@
 class TempLink < ActiveRecord::Base
   include SlugAble
 
+  # TIMEOUT = Settings.dig(:temp_links, :invalidate_after) || 24.hours
+  TIMEOUT = Settings.try(:temp_links).try(:invalidate_after) || 24.hours
+
   belongs_to :picture
+
+  after_create :schedule_invalidation
+
+  private
+
+  def schedule_invalidation
+    InvalidateTempLinks.set(wait: TIMEOUT).perform_later(self)
+  end
 end
