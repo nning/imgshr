ea53fd4657d16ea9df134a8d99184df1e58c23a0
henning mueller <mail@nning.io>
2016-01-20 13:53:52 +0100

    file_releases#create

--- app/controllers/file_releases_controller.rb
+++ app/controllers/file_releases_controller.rb
@@ -1,5 +1,25 @@
 class FileReleasesController < ApplicationController
+  before_action :check_token, only: :create
+
+  protect_from_forgery except: :create
+
   def index
     @file_releases = FileRelease.order('created_at desc').all
+  end
+
+  def create
+    FileRelease.create!(file: params[:file], version: params[:version])
+    head :ok
+  end
+
+  private
+
+  def authorized?
+    token = params[:token]
+    !token.nil? && token == Settings.file_release_token
+  end
+
+  def check_token
+    head :forbidden unless authorized?
   end
 end
