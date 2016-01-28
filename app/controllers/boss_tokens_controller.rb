794b64f72d6afebd7afafd1b214d270181594b35
henning mueller <mail@nning.io>
2016-01-28 21:10:23 +0100

    QR code for boss link.

--- app/controllers/boss_tokens_controller.rb
+++ app/controllers/boss_tokens_controller.rb
@@ -12,13 +12,22 @@ class BossTokensController < ApplicationController
   end
 
   def show
-    if session["boss_token_#{@boss_token.gallery.slug}"].nil?
-      session["boss_token_#{@boss_token.gallery.slug}"] = @boss_token.slug
+    respond_to do |format|
+      format.html do
+        if session["boss_token_#{@boss_token.gallery.slug}"].nil?
+          session["boss_token_#{@boss_token.gallery.slug}"] = @boss_token.slug
+        end
+
+        session["boss_page_visited_#{@boss_token.slug}"] = 1
+
+        redirect_to gallery_path(boss_token.gallery) if params[:redir]
+      end
+
+      format.svg do
+        expires_in 7.days
+        render text: RQRCode::QRCode.new(gallery_delete_url(@boss_token)).as_svg
+      end
     end
-
-    session["boss_page_visited_#{@boss_token.slug}"] = 1
-
-    redirect_to gallery_path(boss_token.gallery) if params[:redir]
   end
 
   private
