e06c48337ccba8da38b7416db3fb27171fdd6f0d
henning mueller <mail@nning.io>
2016-06-05 13:08:29 +0200

    Removed picture nav and old helper methods.

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -3,24 +3,6 @@ module PicturesHelper
     EXIF::FLASH_MAP[picture.flash]
   end
 
-  def lightbox_picture_id(picture, label: false, info: false, hash: false)
-    s = "lightbox#{picture.id}"
-
-    if label
-      s = "label_#{s}"
-    elsif info
-      s = "info_#{s}"
-    end
-
-    s = "##{s}" if hash
-
-    s
-  end
-
-  def nav_for(picture)
-    render 'pictures/nav', picture: picture
-  end
-
   def rating_for(picture)
     render 'pictures/rating', picture: picture if picture.gallery.ratings_enabled
   end
