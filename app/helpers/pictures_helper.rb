bc4617cd373747428aa1e987d5f1b9d3e51180d0
henning mueller <mail@nning.io>
2016-06-18 21:40:40 +0200

    Hide rating if pictures#show and #temp_link.

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -3,15 +3,19 @@ module PicturesHelper
     EXIF::FLASH_MAP[picture.flash]
   end
 
+  def gallery_back_path(picture)
+    gallery_referer?(picture) ? :back : gallery_path(picture.gallery)
+  end
+
+  def hide_gallery?
+    temp_link? || params[:action] == 'show'
+  end
+
   def rating_for(picture)
     render 'pictures/rating', picture: picture if picture.gallery.ratings_enabled
   end
 
   def temp_link?
     params[:action] == 'temp_link'
-  end
-
-  def gallery_back_path(picture)
-    gallery_referer?(picture) ? :back : gallery_path(picture.gallery)
   end
 end
