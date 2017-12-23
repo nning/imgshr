a5e72d2a70b95169f4c256064ed6f62e675d109f
henning mueller <mail@nning.io>
2017-12-23 19:08:24 +0100

    Save nonce alongside image ciphertext

--- app/javascript/utils/crypto.js
+++ app/javascript/utils/crypto.js
@@ -6,40 +6,27 @@ import ReactDOM from 'react-dom'
 import Axios from 'axios'
 import QRCode from '../components/QRCode.jsx'
 
-function serializeSecrets(n, k) {
-  return JSON.stringify({n: n, k: k})
-}
+const NONCE_LENGTH = 24
 
-function getSecrets(nacl) {
+function getKey(nacl) {
   const slug = document.getElementById('gallery').getAttribute('slug')
   const item = slug + '_client_encrypted_key'
 
-  let stored = localStorage.getItem(item)
-
-  let n = new Uint8Array(24)
   let k = new Uint32Array(32)
+  let stored = localStorage.getItem(item)
 
   if (!stored) {
     window.crypto.getRandomValues(k);
-    n = nacl.crypto_secretbox_random_nonce()
-
-    localStorage.setItem(item, serializeSecrets(n, k))
+    localStorage.setItem(item, JSON.stringify(k))
   } else {
-    stored = JSON.parse(stored)
-
-    const storedNonce = stored['n']
-    const storedKey = stored['k']
-
-    for (let i in storedNonce) {
-      if (storedNonce.hasOwnProperty(i)) n[i] = storedNonce[i]
-    }
+    const storedKey = JSON.parse(stored)
 
     for (let i in storedKey) {
       if (storedKey.hasOwnProperty(i)) k[i] = storedKey[i]
     }
   }
 
-  return [n, k]
+  return k
 }
 
 function fetchAndDecryptImages() {
@@ -61,22 +48,33 @@ export function encrypt(file, callback) {
 
   reader.onload = () => {
     naclFactory.instantiate((nacl) => {
-      const [n, k] = getSecrets(nacl)
+      const k = getKey(nacl)
+      const n = nacl.crypto_secretbox_random_nonce()
 
       const encoded = nacl.encode_utf8(reader.result);
       const encrypted = nacl.crypto_secretbox(encoded, n, k)
 
-      if (typeof callback === 'function') callback(encrypted)
+      const nonceAndEncrypted = new Uint8Array(n.length + encrypted.length)
+      nonceAndEncrypted.set(n, 0)
+      nonceAndEncrypted.set(encrypted, NONCE_LENGTH)
+
+      if (typeof callback === 'function') callback(nonceAndEncrypted)
     })
   }
 }
 
 export function decrypt(data, callback) {
   naclFactory.instantiate((nacl) => {
-    const [n, k] = getSecrets(nacl)
+    const k = getKey(nacl)
 
-    const a = new Uint8Array(data)
-    const decrypted = nacl.crypto_secretbox_open(a, n, k)
+    const m0 = new Uint8Array(data)
+    const m1 = new Uint8Array(m0.length - NONCE_LENGTH)
+    m1.set(m0.slice(NONCE_LENGTH), 0)
+
+    const n = new Uint8Array(NONCE_LENGTH)
+    n.set(m0.slice(0, NONCE_LENGTH), 0)
+
+    const decrypted = nacl.crypto_secretbox_open(m1, n, k)
     const decoded = nacl.decode_utf8(decrypted)
 
     if (typeof callback === 'function') callback(decoded)
@@ -85,9 +83,7 @@ export function decrypt(data, callback) {
 
 export function init() {
   naclFactory.instantiate((nacl) => {
-    const [n, k] = getSecrets(nacl)
-
-    ReactDOM.render(<QRCode content={serializeSecrets(n, k)}/>,
+    ReactDOM.render(<QRCode content={JSON.stringify(getKey(nacl))}/>,
     document.getElementById('client_encrypted_key'))
   });
 
