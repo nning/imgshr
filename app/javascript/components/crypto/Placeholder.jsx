98e6bb933e453ef693ec7589b9c567a201efed1e
henning mueller <mail@nning.io>
2017-12-30 00:00:42 +0100

    Show icon

--- app/javascript/components/crypto/Placeholder.jsx
+++ app/javascript/components/crypto/Placeholder.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import Icon from '../Icon.jsx'
+
+
+const ICON_MAP = {
+  fetching: 'download',
+  decrypting: 'lock',
+  'error:fetching': 'remove-circle',
+  'error:decrypting': 'remove-circle'
+}
 
 export default class Placeholder extends React.Component {
   render() {
     return (
       <div className="placeholder">
+        <Icon name={ICON_MAP[this.props.status]}/>
         <div className="title">{this.props.title}</div>
         <div>{this.props.status}</div>
       </div>
