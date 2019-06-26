0ba4f3f9e149fbc2e30b9a2ac272106d4ae2d471
henning mueller <mail@nning.io>
2019-06-26 21:20:30 +0200

    Ignore if ActiveStorage conversion fails

--- db/migrate/20180825105800_convert_to_active_storage.rb
+++ db/migrate/20180825105800_convert_to_active_storage.rb
@@ -1,6 +1,8 @@
 class ConvertToActiveStorage < ActiveRecord::Migration[5.2]
   def up
     Rake::Task['active_storage:migrate'].invoke
+  rescue StandardError => e
+    $stderr.puts(e.full_message)
   end
 
   def down
