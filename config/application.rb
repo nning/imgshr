d7b8b92a88022a3f929ed96d3f9a789f11807617
henning mueller <mail@nning.io>
2020-01-04 21:48:43 +0100

    Deactivate unneeded Rails modules

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,22 @@ Warning[:deprecated] = false
 
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+
+%w(
+  active_record/railtie
+  active_storage/engine
+  action_controller/railtie
+  action_view/railtie
+  active_job/railtie
+  rails/test_unit/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
