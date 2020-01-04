bdba50226196b09a7af963661333d239c0830708
henning mueller <mail@nning.io>
2020-01-04 22:57:52 +0100

    Add updated acts-as-taggable-on migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_112809) do
+ActiveRecord::Schema.define(version: 2020_01_04_215556) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
