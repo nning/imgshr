06ca3b02ee26ff775866079b9a136ef87d7d0b75
henning mueller <mail@nning.io>
2020-01-13 06:53:28 +0100

    Re-activate custom RepresentationsController

---
+++ app/controllers/active_storage/representations_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+if Rails.application.config.active_storage.service == :local
+  module ActiveStorage
+    class RepresentationsController < BaseController
+      include ActiveStorage::SetBlob
+
+      def show
+        expires_in 1.year, public: true
+
+        variant = @blob.representation(params[:variation_key]).processed
+
+        send_data @blob.service.download(variant.key),
+          type: @blob.content_type || DEFAULT_SEND_FILE_TYPE,
+          disposition: 'inline'
+      end
+    end
+  end
+end
