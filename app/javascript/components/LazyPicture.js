2eb392f930d07b3508ab2189959f0ee83e2f3f5e
henning mueller <mail@nning.io>
2020-09-19 12:30:58 +0200

    Try native lazy loading for unencrypted images

--- app/javascript/components/LazyPicture.js
+++ app/javascript/components/LazyPicture.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import LazyLoad from 'react-lazyload'
-
 import Placeholder from './Placeholder'
 
 
@@ -39,48 +37,47 @@ export default class LazyPicture extends React.PureComponent {
 
   render() {
     return (
-      <LazyLoad height={325} once>
-        <React.Fragment>
-          {!this.state.error &&
-            <picture className={this.pictureClass()}>
-              {this.props.srcMobile !== null &&
-                <source
-                  srcSet={this.props.srcMobile}
-                  media="(max-width: 768px)"
-                />
-              }
-
-              <img
-                src={this.props.src}
-                title={this.props.title}
-                alt={this.props.title}
-                onLoad={this.onLoad}
-                onError={this.onError}
+      <React.Fragment>
+        {!this.state.error &&
+          <picture className={this.pictureClass()}>
+            {this.props.srcMobile !== null &&
+              <source
+                srcSet={this.props.srcMobile}
+                media="(max-width: 768px)"
               />
-            </picture>
-          }
+            }
 
-          {this.isFetching() &&
-            <Placeholder
-              status="fetching"
+            <img
+              src={this.props.src}
               title={this.props.title}
-              height={this.props.height}
-              width={this.props.width}
+              alt={this.props.title}
+              onLoad={this.onLoad}
+              onError={this.onError}
+              loading="lazy"
             />
-          }
+          </picture>
+        }
 
-          {this.isError() &&
-            <Placeholder
-              status="error:fetching"
-              icon="hourglass"
-              statusText="Not ready, yet"
-              title={this.props.title}
-              height={this.props.height}
-              width={this.props.width}
-            />
-          }
-        </React.Fragment>
-      </LazyLoad>
+        {this.isFetching() &&
+          <Placeholder
+            status="fetching"
+            title={this.props.title}
+            height={this.props.height}
+            width={this.props.width}
+          />
+        }
+
+        {this.isError() &&
+          <Placeholder
+            status="error:fetching"
+            icon="hourglass"
+            statusText="Not ready, yet"
+            title={this.props.title}
+            height={this.props.height}
+            width={this.props.width}
+          />
+        }
+      </React.Fragment>
     )
   }
 }
