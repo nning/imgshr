a44a8946cb8ea1c80510e3a137fea8ee57f1bb97
henning mueller <mail@nning.io>
2022-10-26 14:32:32 +0200

    Fix duplicate detection

--- app/validators/picture_image_validator.rb
+++ app/validators/picture_image_validator.rb
@@ -1,14 +1,14 @@
 class PictureImageValidator < ActiveModel::Validator
   def validate(record)
     unless record.image_file.attached?
-      record.errors[:base] << 'Image missing'
+      record.errors.add :base, 'Image missing'
       return
     end
 
     return unless record.plain?
 
     if !record.image_file.blob.content_type.starts_with?('image/')
-      record.errors[:base] << 'Wrong file format'
+      record.errors.add :base, 'Wrong file format'
       return
     end
 
@@ -19,7 +19,7 @@ class PictureImageValidator < ActiveModel::Validator
         .where(active_storage_blobs: {checksum: new_checksum})
 
       if existing_pictures.any?
-        record.errors[:base] << "Image already exists in gallery"
+        record.errors.add :base, 'Image already exists in gallery'
         return
       end
     end
--- test/models/picture_test.rb
+++ test/models/picture_test.rb
@@ -11,6 +11,35 @@ class PictureTest < ActiveSupport::TestCase
       subject.save!
       _(subject.image_file).wont_be_nil
     end
+
+    it 'should attach same image in 2 galleries' do
+      subject.image_file.attach(io: emsi(), filename: 'emsi.png')
+      subject.save!
+      _(subject.image_file).wont_be_nil
+
+      another_picture = Gallery.create!.pictures.build
+      another_picture.image_file.attach(io: emsi(), filename: 'emsi.png')
+      another_picture.save!
+      _(another_picture.image_file).wont_be_nil
+
+      _(Picture.count).must_equal(2)
+    end
+
+    it 'should not attach same image in 1 gallery' do
+      subject.image_file.attach(io: emsi(), filename: 'emsi.png')
+      subject.save!
+      _(subject.image_file).wont_be_nil
+
+      another_picture = gallery.pictures.build
+      another_picture.image_file.attach(io: emsi(), filename: 'emsi.png')
+      _(another_picture.validate).must_equal(false)
+
+      begin
+        another_picture.save!
+      rescue
+      end
+      _(gallery.pictures.count).must_equal(1)
+    end
   end
 
   describe 'image analyzer' do
