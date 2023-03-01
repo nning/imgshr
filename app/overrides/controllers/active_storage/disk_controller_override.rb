d6124e8e59d9547acf0f5b06baca9a87085b689d
henning mueller <mail@nning.io>
2023-03-01 23:08:13 +0100

    Try fix failing test

--- app/overrides/controllers/active_storage/disk_controller_override.rb
+++ app/overrides/controllers/active_storage/disk_controller_override.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # See https://github.com/rails/rails/blob/v7.0.4.2/activestorage/app/controllers/active_storage/disk_controller.rb#L12
 
-if [:local, :test].include? Rails.application.config.active_storage.service
+if [:local, :test].include?(Rails.application.config.active_storage.service) && !ActiveStorage::DiskController.instance_methods.include?(:_show)
   ActiveStorage::DiskController.class_eval do
     alias_method :_show, :show
 
