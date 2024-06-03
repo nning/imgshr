5b6efaa51084d2843b608198bfdfe403c95d8d6a
henning mueller <mail@nning.io>
2024-06-03 15:49:38 +0200

    chore: update Ruby and Rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2020_12_28_150503) do
+ActiveRecord::Schema[7.1].define(version: 2020_12_28_150503) do
   create_table "active_storage_attachments", charset: "utf8mb3", collation: "utf8mb3_general_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
