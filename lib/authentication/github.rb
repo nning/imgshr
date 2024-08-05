f84799365173d541bf4b5aaf0b86e0ab199f5ee4
henning mueller <mail@nning.io>
2024-08-05 12:44:34 +0200

    feat: add allowed_github_users setting and check

--- lib/authentication/github.rb
+++ lib/authentication/github.rb
@@ -10,10 +10,19 @@ module Authentication::Github
 
   def self.login?(session)
     config = Settings.authentication.admin.github_login
+    user = session['github_login']
 
     return false unless config
-    return false unless session['github_login']
+    return false unless user
 
-    true
+    self.allowed_github_user?(user)
+  end
+
+  private def self.allowed_github_user?(login)
+    if !Settings.authentication.allowed_github_users.is_a?(Array)
+      return true
+    end
+
+    Settings.authentication.allowed_github_users.include?(session['github_login'])
   end
 end
