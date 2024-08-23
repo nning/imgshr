699796fdb0aa6a3d291f84a5a2c1f43a55e00fde
henning mueller <mail@nning.io>
2024-08-23 09:10:08 +0200

    chore: update to Rails 7.2.1

--- config/initializers/omniauth_github.rb
+++ config/initializers/omniauth_github.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  secrets = Rails.application.secrets
+  secrets = Rails.application.credentials
   provider :github, secrets.github_key, secrets.github_secret
 end
